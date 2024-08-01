d325d29f1c77283cea384c496f964bab62c15c61
Caleb Okwesie Arthur <144965913+Okwesie@users.noreply.github.com>
2024-08-01 14:44:03 +0000

    Update art_gallery.py

--- art_gallery.py
+++ art_gallery.py
@@ -2,6 +2,37 @@ import streamlit as st
 import pandas as pd
 import mysql.connector
 
+# Add this at the beginning of your script
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password incorrect, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+if check_password():
+    
+
 # Connect to your database
 cnx = mysql.connector.connect(
     user="root",
@@ -10,7 +41,7 @@ cnx = mysql.connector.connect(
     port=3307,
     database="artgallery",
 )
-
+                                        
 # Create a cursor object
 cursor = cnx.cursor()
 
